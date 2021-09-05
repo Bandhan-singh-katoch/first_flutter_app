ded89c9f78b898e9801f9b606699ffafc7febcdd
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-05 20:45:10 +0530

    part4 started

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,16 +4,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold
-          ),
-        ),
-      ),
+      child: 
     );
   }
 }
