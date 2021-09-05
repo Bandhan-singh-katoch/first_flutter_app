224c72be72e695533ebf96024882079b305afb28
Bandhan singh <parbhatbandhan@gmail.com>
2021-09-06 00:02:17 +0530

    image added and fontfamily changed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,7 +4,22 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: 
-    );
+      color: Colors.white,
+      child: Column(
+      children: [
+        Image.asset(
+        "assets/images/login_img.png",
+        fit: BoxFit.cover,
+      ),
+
+      Text(
+        "Welcome",style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold
+        ),
+      )
+
+      ],
+    ));
   }
 }
